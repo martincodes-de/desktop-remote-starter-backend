7a4d3d0e2cc98be018d46f0712b8359df1016dfa
martincodes <62392843+martincodes-de@users.noreply.github.com>
2023-02-25 15:24:24 +0100

    add http api

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/martincodes-de/desktop-remote-starter-backend/src"
 	"gopkg.in/ini.v1"
+	"io"
+	"net/http"
 )
 
 func main() {
@@ -18,6 +20,39 @@ func main() {
 	fritzBoxUrl := config.Section("").Key("fritzBoxUrl").String()
 	userName := config.Section("").Key("userName").String()
 	userPassword := config.Section("").Key("userPassword").String()
+	httpApiKey := config.Section("").Key("httpApiKey").String()
+	httpPort := config.Section("").Key("httpPort").String()
 
-	src.TurnOnComputer(isBrowserVisible, fritzBoxUrl, userName, userPassword, "DESKTOP-CCECHMS")
+	http.HandleFunc("/check", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+		io.WriteString(writer, "OK - ONLINE")
+		return
+	})
+
+	http.HandleFunc("/turn-on-computer", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != http.MethodPost {
+			writer.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		var queryParameter = request.URL.Query()
+
+		if queryParameter.Get("apikey") != httpApiKey {
+			writer.WriteHeader(http.StatusForbidden)
+			io.WriteString(writer, "No/wrong apikey provided")
+			return
+		}
+
+		var machineName = queryParameter.Get("machineName")
+		if machineName == "" {
+			writer.WriteHeader(http.StatusBadRequest)
+			io.WriteString(writer, "No machineName provided")
+			return
+		}
+
+		src.TurnOnComputer(isBrowserVisible, fritzBoxUrl, userName, userPassword, machineName)
+	})
+
+	fmt.Println("Server started with port " + httpPort)
+	http.ListenAndServe(":"+httpPort, nil)
 }
